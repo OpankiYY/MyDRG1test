c624b34bc4fe01148b580ae538d0a62699d7b64d
OpankiYY <vborzyaev2222@gmail.com>
2026-02-23 03:26:53 +0300

    Update Third.lua

--- Third.lua
+++ Third.lua
@@ -1,1 +1,7 @@
-print("ThisIsThird")
+wait(90)
+
+getgenv().Hermanos_Settings = {
+    ['key'] = 'd4416c24-8532-4f41-bb40-2df80a33686f',
+    ['PC'] = 'VsPhone1',
+}
+task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/main.lua'))() end
